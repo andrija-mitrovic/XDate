bc3bfcc83e768613ebb31daa892dcd442dd2d621
andrija-mitrovic <57568556+andrija-mitrovic@users.noreply.github.com>
2019-11-20 11:57:59 +0100

    End of section 9

--- XDate-FrontEnd/src/app/members/member-list/member-list.component.ts
+++ XDate-FrontEnd/src/app/members/member-list/member-list.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { User } from 'src/app/_models/user';
+import { UserService } from 'src/app/_services/user.service';
+import { AlertifyService } from 'src/app/_services/alertify.service';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-member-list',
@@ -6,10 +10,14 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./member-list.component.css']
 })
 export class MemberListComponent implements OnInit {
-
-  constructor() { }
+  users: User[];
+  
+  constructor(private userService: UserService, private alertify: AlertifyService, private route: ActivatedRoute) { }
 
   ngOnInit() {
+   this.route.data.subscribe(data => {
+     this.users = data['users']
+   });
   }
 
 }
